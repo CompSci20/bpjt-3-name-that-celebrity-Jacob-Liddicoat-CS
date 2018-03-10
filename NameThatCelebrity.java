196986dac3e6a1a1fd872003ee6d9f1169eac59b
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-03-10 20:11:13 +0000

    Add three println statements

--- NameThatCelebrity.java
+++ NameThatCelebrity.java
@@ -11,3 +11,8 @@ public class NameThatCelebrity
    String s1part2 = s1.substring(2,7);
    String s2part2 = s2.substring(2,7);
    String s3part2 = s3.substring(2,9);
+   System.out.println(s1 + s4 + s1part2); //prints Allan Alda>>>lan A
+   System.out.println(s2 + s4 + s2part2); //prints John Wayne>>>hn Wa
+   System.out.println(s3 + s4 + s3part2); //prints Gregory Peck>>>egory P
+  }
+ }
