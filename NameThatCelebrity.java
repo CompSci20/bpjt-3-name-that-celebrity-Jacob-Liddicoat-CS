3027747eb4ce63ded9caceac1dd587d904db9c36
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-03-10 19:57:50 +0000

    Add three substrings

--- NameThatCelebrity.java
+++ NameThatCelebrity.java
@@ -7,3 +7,6 @@ public class NameThatCelebrity
    String s1 ="Allan Alda";
    String s2 ="John Wayne";
    String s3 ="Gregory Peck";
+   String s1part2 = s1.substring(2,7);
+   String s2part2 = s2.substring(2,7);
+   String s3part2 = s3.substring(2,9);
